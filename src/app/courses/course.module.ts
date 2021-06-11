0d039f9b1c02960cd6bf56589db055dc66010320
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-11 01:19:21 -0300

    Concluindo a exportação da pasta Pipe e Star

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
 import { StarModule } from "../shared/component/star/star.module";
+import { AppPipeModule } from "../shared/pipe/app-pipe-module";
 
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
@@ -12,7 +12,6 @@ import { CourseListComponent } from "./course-list.component";
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
        
         
     ],
@@ -20,6 +19,7 @@ import { CourseListComponent } from "./course-list.component";
         CommonModule,
         FormsModule,
         StarModule,
+        AppPipeModule,
         RouterModule.forChild([
               {
                 path: 'courses/info/:id', component: CourseInfoComponent
