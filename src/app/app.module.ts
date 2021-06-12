9687fce71ec606882493a9ac82469eb0e273a71c
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-11 21:03:34 -0300

    Pasta Core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 import { Error404Component } from './error404/error404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component
+    Error404Component,
+    
    
   ],
   imports: [
@@ -24,6 +26,7 @@ import { CourseModule } from './courses/course.module';
     FormsModule,
     CourseModule,
     HttpClientModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
